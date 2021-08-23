5f060ef92b7559687e6bde51b02c052ce675f3cd
Thibaud LARIVIERE <thibaud.lariviere@effidence.com>
2021-08-23 16:55:31 +0200

    Configure typescript
    Transform express default skeleton to ts files.

---
+++ src/server.ts
@@ -0,0 +1,69 @@
+import debugModule from "debug";
+import http from "http";
+import app from "./app";
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val: string) {
+  const port = parseInt(val, 10);
+
+  if (Number.isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+interface NodeError extends Error {
+  code: string;
+}
+interface SystemError extends NodeError {
+  syscall: string;
+}
+function isSystemError(error: NodeError): error is SystemError {
+  return (error as SystemError).syscall !== undefined;
+}
+
+const debug = debugModule("login:server");
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+
+server.on("error", (error: NodeError) => {
+  if (!isSystemError(error) || error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind =
+    typeof port === "string" ? `Pipe ${port}` : `Port ${port.toString()}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+});
+
+server.on("listening", () => {
+  const addr = server.address();
+  if (addr === null) {
+    return;
+  }
+
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+});
+
+server.listen(port);
