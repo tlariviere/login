f01bf3be024bd0d9d7bd0c4011b044da7a012c0b
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2021-09-27 18:51:40 +0200

    Remove unused files

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,6 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import path from "path";
 
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
-
 const app = express();
 
 app.use(logger("dev"));
@@ -13,8 +10,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 export default app;
