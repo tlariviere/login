c1214c81142be7bdf1f019d27b67a83cae462c5f
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2021-09-28 12:03:59 +0200

    Implement sign-in POST request handler
    Require request body to be parsed by bodyParser. Request body should
    include fields "login" (either username or email) and "password".
    In case of success, return status 200 with access_token and
    refresh_token cookies and with user unprotected data in response body.

---
+++ src/auth/router/userUnprotectedData.ts
@@ -0,0 +1,12 @@
+import type { User, UserUnprotectedData } from "../types";
+
+/**
+ * Filter out sensible user data.
+ */
+const userUnprotectedData = <Roles extends string>(
+  user: User<Roles>
+): UserUnprotectedData<Roles> => {
+  return { name: user.name, email: user.email, role: user.role };
+};
+
+export default userUnprotectedData;
---
+++ src/auth/testingUtils/MockFindUser.ts
@@ -0,0 +1,29 @@
+import type { User } from "../types";
+
+export default class MockFindUser<Roles extends string> {
+  public byId = jest.fn();
+  public byLogin = jest.fn();
+
+  public mockResolvedUserIfMatch(user: User<Roles>): void {
+    this.byId.mockImplementation((id) =>
+      Promise.resolve(id === user.id ? user : undefined)
+    );
+    this.byLogin.mockImplementation((usernameOrEmail) =>
+      Promise.resolve(
+        usernameOrEmail === user.name || usernameOrEmail === user.email
+          ? user
+          : undefined
+      )
+    );
+  }
+
+  public mockClear(): void {
+    this.byId.mockClear();
+    this.byLogin.mockClear();
+  }
+
+  public mockReset(): void {
+    this.byId.mockReset();
+    this.byLogin.mockReset();
+  }
+}
