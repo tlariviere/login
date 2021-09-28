d0ac750648ba41df6cdc28d4fdea7536d1e3fa2d
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2021-09-28 12:52:37 +0200

    Implement access token refresh POST request handler
    Require request cookies to be parsed by tough-cookie.
    On success, return status 200 with access_token and refresh_token
    cookies.

---
+++ src/auth/router/refresh.test.ts
@@ -0,0 +1,73 @@
+import type { Response } from "express";
+
+import type { Request } from "../../utils/types";
+import type TokenFamilies from "../TokenFamilies";
+import MockTockenFamily from "../testingUtils/MockTokenFamily";
+import MockTokenFamilies from "../testingUtils/MockTokenFamilies";
+import MockResponse from "../testingUtils/MockResponse";
+import refresh from "./refresh";
+
+describe("Auth router token refresh", () => {
+  type Roles = string;
+  const accessToken = "access";
+  const refreshToken = "refresh";
+  const tokenFamily = new MockTockenFamily(accessToken, refreshToken);
+  const tokenFamilies = new MockTokenFamilies(tokenFamily);
+  const cookieOptions = { secure: false, httpOnly: true, sameSite: true };
+  const handler = refresh(
+    tokenFamilies as unknown as TokenFamilies<Roles>,
+    cookieOptions
+  );
+  const res = new MockResponse();
+  const next = jest.fn();
+
+  beforeAll(() => {
+    tokenFamilies.mockVerifyRefreshToken("foo");
+  });
+
+  beforeEach(() => {
+    res.mockClear();
+  });
+
+  test("should fail on missing refresh_token cookie", async () => {
+    const req = { cookies: {} } as unknown as Request;
+    await handler(req, res as unknown as Response, next);
+    expect(res.statusCode).toBe(401);
+  });
+
+  test("should fail on invalid refresh token", async () => {
+    const req = {
+      cookies: { refresh_token: refreshToken },
+    } as unknown as Request;
+    const errorMsg = "bar";
+    tokenFamilies.mockVerifyRefreshTokenOnce(new Error(errorMsg));
+    await handler(req, res as unknown as Response, next);
+    expect(res.statusCode).toBe(401);
+    expect(res.send).toHaveBeenCalledWith(errorMsg);
+  });
+
+  test("should return status 200 on success", async () => {
+    const req = {
+      cookies: { refresh_token: refreshToken },
+    } as unknown as Request;
+    await handler(req, res as unknown as Response, next);
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("should set access and refresh token as cookies", async () => {
+    const req = {
+      cookies: { refresh_token: refreshToken },
+    } as unknown as Request;
+    await handler(req, res as unknown as Response, next);
+    expect(res.cookie).toHaveBeenCalledWith(
+      "access_token",
+      accessToken,
+      cookieOptions
+    );
+    expect(res.cookie).toHaveBeenCalledWith(
+      "refresh_token",
+      refreshToken,
+      cookieOptions
+    );
+  });
+});
---
+++ src/auth/router/refresh.ts
@@ -0,0 +1,45 @@
+import type { CookieOptions } from "express";
+
+import type { AsyncRequestHandler } from "../../utils/types";
+import type { AuthenticateReq } from "../types";
+import TokenFamilies from "../TokenFamilies";
+
+/**
+ * POST request handler to refresh access token.
+ * If successfull, generate new access and refresh token couple
+ * and send them as cookies.
+ * Require request cookies to be parsed by tough-cookie.
+ * @param tokenFamilies Function to send email verification when signing up new user.
+ * @param cookieOptions Cookie options to store auth token.
+ * @returns Express request handler.
+ */
+const refresh = <Roles extends string>(
+  tokenFamilies: TokenFamilies<Roles>,
+  cookieOptions: CookieOptions
+): AsyncRequestHandler => {
+  return async (req: AuthenticateReq<Roles>, res) => {
+    const { refresh_token: oldRefreshToken } = req.cookies;
+    if (!oldRefreshToken) {
+      res.sendStatus(401);
+      return;
+    }
+
+    try {
+      const { family: tokenFamily, body } =
+        await tokenFamilies.verifyRefreshToken(oldRefreshToken);
+      const { accessToken, refreshToken } = tokenFamily.generateTokens(
+        body.sub,
+        body.role
+      );
+      res
+        .status(200)
+        .cookie("access_token", accessToken, cookieOptions)
+        .cookie("refresh_token", refreshToken, cookieOptions)
+        .send();
+    } catch (err) {
+      res.status(401).send((err as Error).message);
+    }
+  };
+};
+
+export default refresh;
