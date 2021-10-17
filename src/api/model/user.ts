389d9f9f64786f6b6da5824b3282ae023e98013e
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2021-10-17 13:27:02 +0200

    Add mongoDB User model

---
+++ src/api/model/user.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+
+import type { User } from "../../auth/types";
+import type { Roles } from "../strategy/roles";
+import { roles } from "../strategy/roles";
+
+const emailRegex = /^[a-z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-z0-9.-]+$/;
+
+const schema = new Schema({
+  name: { type: String, minLength: 3, trim: true, unique: true },
+  email: { type: String, lowercase: true, match: emailRegex, unique: true },
+  hashedPassword: { type: String, required: true },
+  role: { type: String, enum: roles, default: "user" },
+});
+
+export default model<User<Roles>>("user", schema);
