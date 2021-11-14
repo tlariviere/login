9abeded314f9f1dbd1a1b4df060d72d6d7e6702d
Thibaud LARIVIERE <lariviere.thibaud@gmail.com>
2021-11-14 15:03:20 +0100

    [Example app] Set up webpack for browser example app

--- packages/example/src/server.ts
+++ packages/example/src/server.ts
@@ -1,10 +1,14 @@
+import path from "path";
 import http from "http";
 import express from "express";
 import logger from "morgan";
 import helmet from "helmet";
+import webpack from "webpack";
+import webpackDevMiddleware from "webpack-dev-middleware";
 import config from "./constants/server";
 
 import type { NodeError } from "./utils/types";
+import webpackConfig from "./webpack.config";
 import apiRouter from "./api";
 import exitWithError from "./utils/exitWithError";
 import { isSystemError } from "./utils/types";
@@ -14,6 +18,17 @@ const app = express();
 app.use(logger("dev"));
 app.use(helmet());
 app.use("/api", apiRouter);
+
+if (process.env.NODE_ENV === "development") {
+  const compiler = webpack(webpackConfig as webpack.Configuration);
+  app.use(
+    webpackDevMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+    })
+  );
+} else {
+  app.get("/*", express.static(path.join(__dirname, "public")));
+}
 
 const server = http.createServer(app);
 
