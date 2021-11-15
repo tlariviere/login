2ad562019257991665d0ef433bfb9962bf6dd878
Thibaud LARIVIERE <thibaud.lariviere@effidence.com>
2021-11-15 12:13:47 +0100

    [Example app] Implement sign-out

--- packages/example/src/components/SignOut.tsx
+++ packages/example/src/components/SignOut.tsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { useQueryClient } from "react-query";
 
+import useMutationRequireLogin from "../hooks/useMutationRequireLogin";
+import { fetchData } from "../utils/fetchHelpers";
 import "../styles/DarkButton.scss";
 
-const SignOut: React.FC = () => (
-  <button type="button" className="DarkButton">
-    Sign out
-  </button>
-);
+const SignOut: React.FC = () => {
+  const queryClient = useQueryClient();
+  const { isLoading, mutate } = useMutationRequireLogin(
+    "sign-out",
+    () => fetchData("/api/auth/sign-out", { method: "POST" }),
+    { onSuccess: () => queryClient.resetQueries("userInfo", { exact: true }) }
+  );
+
+  return (
+    <button
+      type="button"
+      className="DarkButton"
+      onClick={() => mutate()}
+      disabled={isLoading}
+    >
+      Sign out
+    </button>
+  );
+};
 
 export default SignOut;
